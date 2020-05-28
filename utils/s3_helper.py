9a754750dcca4777d4bf4c0ec4befa0b4ab46b1d
Martin Müller <martin.mathias.mueller@gmail.com>
2020-05-28 18:34:28 +0200

    make sync compatible with gz file types, fix logging

--- utils/s3_helper.py
+++ utils/s3_helper.py
@@ -11,6 +11,7 @@ import joblib
 import multiprocessing
 from datetime import datetime, timedelta
 
+logger = logging.getLogger(__name__)
 
 class S3Helper:
     def __init__(self, bucket_name='crowdbreaks-prd', profile='default'):
@@ -18,7 +19,6 @@ class S3Helper:
         boto3.setup_default_session(profile_name=profile)
         self.client = boto3.client('s3')
         self.bucket = boto3.resource('s3').Bucket(self.bucket_name)
-        self.logger = logging.getLogger(__name__)
 
     def sync(self, project_name, data_type='all', last_n_days=None):
         t_start = time.time()
@@ -30,7 +30,7 @@ class S3Helper:
         if data_type == 'all' or data_type == 'media':
             self.sync_media_data(project_name)
         t_end = time.time()
-        self.logger.info('Finished data sync in {:.1f} min.'.format((t_end - t_start)/60))
+        logger.info('Finished data sync in {:.1f} min.'.format((t_end - t_start)/60))
 
     def sync_project_info(self, project_name):
         project_fname = os.path.join(find_project_root(), 'project_info.json')
@@ -40,24 +40,27 @@ class S3Helper:
                 if not os.path.isfile(project_fname):
                     # no local file present
                     self.download_file(project_file, project_fname)
-                    self.logger.info('Successfully initialized project "{}". Find project config file under "{}".'.format(project_name, project_fname))
+                    logger.info('Successfully initialized project "{}". Find project config file under "{}".'.format(project_name, project_fname))
                 elif not self.is_up_to_date(project_file, project_fname):
                     # local file present, but not up-to-date. remove old local file and download new
                     os.remove(project_fname)
                     self.download_file(project_file, project_fname)
-                    self.logger.info('Successfully updated project config "{}".'.format(project_fname))
+                    logger.info('Successfully updated project config "{}".'.format(project_fname))
                 else:
                     # is up-to-date
-                    self.logger.info('Project config file is up-to-date.')
+                    logger.info('Project config file is up-to-date.')
                 return
-        self.logger.error('Could not find project "{}" remotely.".'.format(project_name))
+        logger.error('Could not find project "{}" remotely.".'.format(project_name))
 
     def sync_streaming_data(self, project_name, last_n_days=10):
-        self.logger.info('Syncing streaming data ...')
+        logger.info('Syncing streaming data ...')
         local_tweets_dir = os.path.join(find_project_root(), 'data', '0_raw', 'streaming')
-        local_files = [os.path.basename(p) for p in glob.glob(os.path.join(local_tweets_dir, '*.jsonl'))]
+        local_files = []
+        for file_type in ['jsonl', 'gz']:
+            globbed = glob.glob(os.path.join(local_tweets_dir, f'*.{file_type}'))
+            local_files.extend([os.path.basename(l) for l in globbed])
         if last_n_days is not None:
-            self.logger.info('... of the last {} days...'.format(last_n_days))
+            logger.info('... of the last {} days...'.format(last_n_days))
             today = datetime.now()
             sync_prefixes = []
             total_files_synced = 0
@@ -69,19 +72,19 @@ class S3Helper:
                 _, num_files_synced = self.download_files(key_prefix, local_tweets_dir, local_files, parallel=True)
                 total_files_synced += num_files_synced
             if num_files_synced == 0:
-                self.logger.info('Nothing to sync - Everything up-to-date.')
+                logger.info('Nothing to sync - Everything up-to-date.')
         else:
             key_prefix = 'tweets/{}/'.format(project_name)
             remote_files, num_files_synced = self.download_files(key_prefix, local_tweets_dir, local_files, parallel=True)
             # remove old files
             num_files_removed = self.remove_old_files(remote_files=remote_files, local_files=local_files, local_dir=local_tweets_dir)
             if num_files_synced == num_files_removed == 0:
-                self.logger.info('Everything up-to-date.')
+                logger.info('Everything up-to-date.')
 
     def sync_annotation_data(self, project_name):
-        self.logger.info('Syncing annotation data ...')
+        logger.info('Syncing annotation data ...')
         for mode in ['local', 'mturk', 'public', 'other']:
-            self.logger.info('... of type {}...'.format(mode))
+            logger.info('... of type {}...'.format(mode))
             mode_prefix = mode + '-batch-job' if mode in ['local', 'mturk'] else mode
             key_prefix = 'other/csv/{}/{}-results'.format(project_name, mode_prefix)
             annotation_dir = os.path.join(find_project_root(), 'data', '3_labelled', mode)
@@ -99,17 +102,17 @@ class S3Helper:
                 remote_files, t_num_files_synced = self.download_files(key_prefix, batch_dir, local_files)
                 t_num_files_removed = self.remove_old_files(remote_files=remote_files, local_files=local_files, local_dir=batch_dir)
                 if r_num_files_synced == r_num_files_removed == t_num_files_synced == t_num_files_removed == 0:
-                    self.logger.info('Everything up-to-date.')
+                    logger.info('Everything up-to-date.')
 
     def sync_media_data(self, project_name):
-        self.logger.info('Syncing media data ...')
+        logger.info('Syncing media data ...')
         key_prefix = 'media/{}/'.format(project_name)
         local_dir = os.path.join(find_project_root(), 'data', '0_raw', 'media')
         local_files = [os.path.basename(p) for p in glob.glob(os.path.join(os.path.join(local_dir, '*')))]
         remote_files, r_num_files_synced = self.download_files(key_prefix, local_dir, local_files)
         r_num_files_removed = self.remove_old_files(remote_files=remote_files, local_files=local_files, local_dir=local_dir)
         if r_num_files_synced == r_num_files_removed == 0:
-            self.logger.info('Everything up-to-date.')
+            logger.info('Everything up-to-date.')
 
     def download_files(self, key_prefix, download_dir, local_files, parallel=True):
         def download_file(bucket_name, key, download_dir):
@@ -184,7 +187,7 @@ class S3Helper:
         files_to_be_removed = [os.path.join(local_dir, p) for p in files_to_be_removed]
         num_files = len(files_to_be_removed)
         if num_files > 0:
-            self.logger.info('Removing {} old files...'.format(num_files))
+            logger.info('Removing {} old files...'.format(num_files))
         for f in files_to_be_removed:
             os.remove(f)
         return len(files_to_be_removed)
