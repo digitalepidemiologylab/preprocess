bc4265a82ffe7b52caac0ba14d067e41527ab9cd
Martin Müller <martin.mathias.mueller@gmail.com>
2020-06-16 15:47:39 +0200

    fix prepare pretrain script

--- scripts/prepare_pretrain_data.py
+++ scripts/prepare_pretrain_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import sys
 sys.path.append('..')
-from utils.helpers import get_all_data, find_folder
+from utils.helpers import get_parsed_data, find_folder
 from utils.misc import ArgParseDefault
 import logging
 from tqdm import tqdm, trange
@@ -10,6 +10,7 @@ import re
 import joblib
 import multiprocessing
 import datetime
+from utils.process_tweet import ProcessTweet
 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)-5.5s] [%(name)-12.12s]: %(message)s')
@@ -25,45 +26,40 @@ def main(args):
     """
     # load data
     logger.info('Reading data...')
-    usecols = ['id', 'text', 'lang', 'is_duplicate', 'token_count', 'is_retweet', 'contains_keywords']
-    df = get_all_data(dtype=args.dtype, include_flags=True, include_predictions=False, include_cleaned_labels=False, usecols=usecols, nrows=args.nrows)
+    usecols = ['id', 'text', 'lang', 'token_count', 'is_retweet', 'contains_keywords']
+    df = get_parsed_data(usecols=usecols, num_files=args.num_files)
     logger.info(f'...loaded a total of {len(df):,} tweets')
 
-    filters = {'contains_keywords': 'contains_keywords'}
-    filters_not = {'retweets': 'is_retweet', 'training_data': 'L', 'is_duplicate': 'is_duplicate'}
-
-    # Filtering by col
-    for _filter in args.filters:
-        logger.info(f'Applying filter {_filter}')
+    # Filter retweets
+    if 'retweets' in args.filters:
+        logger.info(f'Filter retweets...')
         num_before = len(df)
-        if _filter in filters:
-            df = df[df[filters[_filter]]]
-        elif _filter in filters_not:
-            df = df[~df[filters_not[_filter]]]
-        else:
-            raise ValueError(f'Unknown filter {_filter}')
+        df = df[~df.is_retweet]
         num_after = len(df)
         logger.info(f'... {num_after:,} remaining (removed {num_before-num_after:,})')
 
-    logger.info(f'Filter lang {args.lang}...')
-    num_before = len(df)
-    df = df[df.lang == args.lang]
-    num_after = len(df)
-    logger.info(f'... {num_after:,} remaining (removed {num_before-num_after:,})')
+    # Filtering by keyword
+    if 'contains_keywords' in args.filters:
+        logger.info(f'Filter contains_keywords...')
+        num_before = len(df)
+        df = df[df.contains_keywords]
+        num_after = len(df)
+        logger.info(f'... {num_after:,} remaining (removed {num_before-num_after:,})')
 
-    logger.info(f'Filter has >={args.min_tokens} tokens...')
-    df = df[df.token_count >= args.min_tokens]
-    num_after = len(df)
-    logger.info(f'... {num_after:,} remaining (removed {num_before-num_after:,})')
+    # filter lang
+    if args.lang is not None:
+        logger.info(f'Filter lang {args.lang}...')
+        num_before = len(df)
+        df = df[df.lang == args.lang]
+        num_after = len(df)
+        logger.info(f'... {num_after:,} remaining (removed {num_before-num_after:,})')
 
-    if args.dtype == 'anonymized':
-        logger.info('Replacing user handles...')
-        # make sure there are no user handles left
-        df.loc[:, 'text'] = df.text.apply(replace_user_handles)
-
-    # standardize text
-    logger.info('Standardize text...')
-    df.loc[:, 'text'] = df.text.apply(standardize_text)
+    # filter min tokens
+    if args.min_tokens > 0:
+        logger.info(f'Filter has >={args.min_tokens} tokens...')
+        df = df[df.token_count >= args.min_tokens]
+        num_after = len(df)
+        logger.info(f'... {num_after:,} remaining (removed {num_before-num_after:,})')
 
     # generate text column to filter for duplicates
     logger.info('Remove duplicates...')
@@ -96,7 +92,7 @@ def main(args):
         write_output_file_delayed = joblib.delayed(write_output_file)
         res = parallel((write_output_file_delayed(
             _df.iloc[i:(i+args.max_examples_per_file)], 
-            os.path.join(output_folder, f'pretrain_{args.dtype}_{_type}_{j:03}.txt')
+            os.path.join(output_folder, f'pretrain_{_type}_{j:03}.txt')
             ) for j, i in enumerate(trange(0, len(_df), args.max_examples_per_file))))
         logger.info(f'Successfully wrote {len(res):,} file(s) to folder {output_folder}')
 
@@ -108,35 +104,23 @@ def write_output_file(df, f_path):
     return 1
 
 def generate_text_cleared(text):
-    # Remove RT @<user>, @<user> and <url>
-    text = re.sub(r'RT @<user>:|@<user>|<url>', '', text)
-    # Remove induced whitespaces
-    text = ' '.join(text.split())
+    # anonymize text
+    text = ProcessTweet.anonymize_text(text)
     # lowercase everything
     text = text.lower()
-    return text
-
-def standardize_text(text):
-    """Replace some non-standard characters such as ” or ’ with standard characters. """
-    text = text.translate(transl_table)
-    # Sometimes user handles seem to contain an additional @ which will lead to @@<user> during anonymization
-    text = text.replace('@@<user>', '@<user>')
-    # replace multiple spaces with single space
+    # Remove RT @<user>, @<user> and <url>
+    text = re.sub(r'rt @user:|@user|<url>', '', text)
+    # Remove induced whitespaces
     text = ' '.join(text.split())
-    return text
-
-def replace_user_handles(text):
-    text = re.sub(user_handle_regex, '@<user>', text)
     return text
 
 def parse_args():
     parser = ArgParseDefault()
-    parser.add_argument('--filters', choices=['retweets', 'training_data', 'is_duplicate', 'contains_keywords'], default=['retweets', 'training_data', 'is_duplicate'], help="Apply filters")
-    parser.add_argument('--dtype', default='anonymized', choices=['original', 'anonymized', 'encrypted'], help="Data type")
+    parser.add_argument('--filters', choices=['retweets', 'contains_keywords'], default=['retweets'], help="Apply filters")
     parser.add_argument('--lang', default='en', help="Filter language")
     parser.add_argument('--min_tokens', default=3, help="Min num tokens")
     parser.add_argument('--max_examples_per_file', default=int(1e6), type=int, help="Max examples per file")
-    parser.add_argument('--nrows', default=None, type=int, help="Only read n rows from file")
+    parser.add_argument('--num_files', default=None, type=int, help="Only read n files from file")
     parser.add_argument('--no_parallel', action='store_true', default=False, help='Do not run in parallel')
     args = parser.parse_args()
     return args
