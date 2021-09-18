9b6fabfc6d8877e1216a0816246eac18db644b58
François Pichard <francois.pichard@epfl.ch>
2021-09-18 13:16:47 +0200

    Select raw data with the new name format

--- utils/processing/parse_tweets.py
+++ utils/processing/parse_tweets.py
@@ -67,7 +67,7 @@ import gzip
 import bz2
 import ray
 import sys
-
+import re
 
 logger = logging.getLogger(__name__)
 output_folder = os.path.join('data', '1_parsed')
@@ -106,17 +106,20 @@ def generate_file_list(extend=False, omit_last_day=True):
     if extend:
         used_files = read_used_files()
     for f_name in f_names:
-        if os.path.basename(f_name).startswith('tweets'):
-            date_str = f_name.split('-')[1]
-            day_str = datetime.strptime(date_str, '%Y%m%d%H%M%S').strftime(datefmt)
-        else:
-             day_str = '-'.join(f_name.split('/')[-5:-2])
+        # if os.path.basename(f_name).startswith('tweets'):
+        #     date_str = f_name.split('-')[1]
+        #     day_str = datetime.strptime(date_str, '%Y%m%d%H%M%S').strftime(datefmt)
+        if os.path.basename(f_name).startswith('crowdbreaks'):
+            date_str = re.findall(r'\d{4}\-\d+\-\d+\-\d+\-\d+\-\d+', os.path.basename(f_name))[0]
+            day_str = datetime.strptime(date_str, '%Y-%m-%d-%H-%M-%S').strftime(datefmt)
+        # else:
+        #      day_str = '-'.join(f_name.split('/')[-5:-2])
         if extend:
             if day_str in used_files and f_name in used_files[day_str]:
                 continue
         grouped_f_names[day_str].append(f_name)
     if omit_last_day:
-        # remove last incomplete day (this makes extending data easier)
+        # Remove last incomplete day (this makes extending data easier)
         if len(grouped_f_names) > 0:
             max_date = max([datetime.strptime(key, datefmt) for key in grouped_f_names.keys()])
             max_date_key = max_date.strftime(datefmt)
@@ -129,42 +132,42 @@ def generate_file_list(extend=False, omit_last_day=True):
 def write_parquet_file(f_path_intermediary, interaction_counts, extend=False):
     key = f_path_intermediary.split('/')[-1].split('.jsonl')[0]
     f_out = os.path.join(output_folder, 'tweets', f'parsed_{key}.parquet')
-    # read from json lines
+    # Read from json lines
     dtypes = get_dtypes()
     df = pd.read_json(f_path_intermediary, lines=True, dtype=dtypes)
     if len(df) == 0:
         return 0
-    # drop duplicates
+    # Drop duplicates
     df.drop_duplicates(subset=['id'], inplace=True)
     # read_json converts null to stringified 'None', convert manually
     for col in [c for c, v in dtypes.items() if v == str]:
         df.loc[df[col] == 'None', col] = None
-    # sanity check, verify uniqueness of IDs
+    # Sanity check, verify uniqueness of IDs
     df = df.drop_duplicates(subset=['id'])
-    # merge with interaction counts
+    # Merge with interaction counts
     if len(interaction_counts) > 0:
-        # subsetting interaction counts to save memory during merge
+        # Subsetting interaction counts to save memory during merge
         interaction_counts_new = interaction_counts[interaction_counts.id.isin(df.id.unique())]
         df = df.merge(interaction_counts_new, on='id', how='left')
         for col in ['num_replies', 'num_quotes', 'num_retweets']:
             df[col] = df[col].fillna(0).astype(int)
     else:
-        # set default values
+        # Set default values
         for col in ['num_replies', 'num_quotes', 'num_retweets']:
             df[col] = 0
-    # convert columns to datetime
+    # Convert columns to datetime
     for datetime_col in ['created_at', 'user.created_at']:
         df[datetime_col] = pd.to_datetime(df[datetime_col])
-    # convert to categorical types
+    # Convert to categorical types
     for col in ['country_code', 'region', 'subregion', 'location_type', 'geo_type', 'lang']:
         df[col] = df[col].astype('category')
-    # sort by created_at
+    # Sort by created_at
     df.sort_values('created_at', inplace=True, ascending=True)
     df.reset_index(drop=True, inplace=True)
     if extend:
-        # read pre-existing parquet files
+        # Read pre-existing parquet files
         df_existing = pd.read_parquet(f_out)
-        # sum up previous and new interaction counts
+        # Sum up previous and new interaction counts
         interaction_counts_new = interaction_counts[interaction_counts.id.isin(df_existing.id.unique())]
         merge_cols = ['id', 'num_replies', 'num_quotes', 'num_retweets']
         df_counts = pd.merge(df_existing[merge_cols], interaction_counts_new, on='id', how='left').fillna(0)
@@ -173,16 +176,16 @@ def write_parquet_file(f_path_intermediary, interaction_counts, extend=False):
             df_counts[col] = df_counts[sum_cols].sum(axis=1)
             df_counts[col] = df_counts[col].astype(int)
             df_existing[col] = df_counts[col].values
-        df_counts = None  # free up memory
-        # append newly collected data
+        df_counts = None  # Free up memory
+        # Append newly collected data
         df = pd.concat([df_existing, df])
-        df_existing = None  # free up memory
-        # after concatenation new data is at the bottom and duplicates will be removed from new data
+        df_existing = None  # Free up memory
+        # After concatenation new data is at the bottom and duplicates will be removed from new data
         df = df.drop_duplicates(subset=['id'], keep='first')
-        # sort again
+        # Sort again
         df.sort_values('created_at', inplace=True, ascending=True)
         df.reset_index(drop=True, inplace=True)
-    # write parquet file
+    # Write parquet file
     df.to_parquet(f_out)
     return len(df)
 
@@ -211,12 +214,13 @@ def run(no_parallel=False, extract_retweets=True, extract_quotes=True, extend=Fa
         gc = Geocode()
         gc.load()
         map_data = load_map_data()
-        # create dirs
+        # Create dirs
         for subfolder in ['tweets', 'preliminary']:
             folder = os.path.join(output_folder, subfolder)
             if not os.path.isdir(folder):
                 os.makedirs(folder)
         def write_to_file(obj):
+            # extracted_tweet, which is an instance of the ProcessTweet class, is the object represented by the variable obj
             created_at_day = obj['created_at'][:10]
             f_path = os.path.join(output_folder, 'preliminary', f'{created_at_day}.jsonl')
             with open(f_path, 'a') as f_out:
@@ -238,10 +242,10 @@ def run(no_parallel=False, extract_retweets=True, extract_quotes=True, extend=Fa
                     if isinstance(line, bytes):
                         # gzip.open will provide file in bytes format, in this case deocde
                         line = line.decode()
-                    # some files use single quotation (Python dictionaries), for this we need to use ast.literal_eval
+                    # Some files use single quotation (Python dictionaries), for this we need to use ast.literal_eval
                     tweet = ast.literal_eval(line)
                 except:
-                    # sometimes parsing completely fails
+                    # Sometimes parsing completely fails
                     logger.error('Error parsing line:')
                     logger.error(line)
                     continue
@@ -249,18 +253,18 @@ def run(no_parallel=False, extract_retweets=True, extract_quotes=True, extend=Fa
                     continue  # API logs, error messages, etc.
                 tweet_id = tweet['id_str']
                 if tweet_id in originals:
-                    # skip duplicates
+                    # Skip duplicates
                     continue
-                # flag tweet ID as "used"
+                # Flag tweet ID as "used"
                 originals[tweet_id] = True
-                # extract tweet
+                # Extract tweet
                 pt = ProcessTweet(tweet=tweet, keywords=keywords, map_data=map_data, gc=gc)
                 if ((extract_retweets and pt.is_retweet)                              # extract retweets (optional)
                         or (extract_quotes and pt.has_quote and not pt.is_retweet)    # extract quotes if not retweet of a quote (optional)
                         or (not pt.is_retweet and not pt.has_quote)):                 # always extract original tweets which are neither retweets nor quotes
                     extracted_tweet = pt.extract()
                     write_to_file(extracted_tweet)
-                # add interaction counts
+                # Add interaction counts
                 if pt.is_reply:
                     if pt.replied_status_id in replies_counts:
                         replies_counts[pt.replied_status_id] += 1
@@ -274,7 +278,7 @@ def run(no_parallel=False, extract_retweets=True, extract_quotes=True, extend=Fa
                         else:
                             quote_counts[pt_quote.id] = 1
                     if not pt_quote.id in originals:
-                        # extract original status
+                        # Extract original status
                         originals[pt_quote.id] = True
                         extracted_tweet = pt_quote.extract()
                         write_to_file(extracted_tweet)
@@ -285,12 +289,12 @@ def run(no_parallel=False, extract_retweets=True, extract_quotes=True, extend=Fa
                     else:
                         retweet_counts[pt_retweet.id] = 1
                     if pt_retweet.id not in originals:
-                        # extract original status
+                        # Extract original status
                         originals[pt_retweet.id] = True
                         extracted_tweet = pt_retweet.extract()
                         write_to_file(extracted_tweet)
             f.close()
-    # setup
+    # Setup
     s_time = time.time()
     try:
         project_info = get_project_info()
@@ -299,7 +303,7 @@ def run(no_parallel=False, extract_retweets=True, extract_quotes=True, extend=Fa
         logger.warning('Could not find project info file. Will not compute matching keywords')
         keywords = []
 
-    # collect file list
+    # Collect file list
     grouped_f_names = generate_file_list(extend=extend, omit_last_day=omit_last_day)
 
     if len(grouped_f_names) == 0:
@@ -314,12 +318,12 @@ def run(no_parallel=False, extract_retweets=True, extract_quotes=True, extend=Fa
     if not extend and len(existing_parquet_files) > 0:
         raise Exception('Found existing parsed files. Either delete pre-existing data manually or pass --extend argument.')
 
-    # make sure map data is downloaded and geocode is set up
+    # Make sure map data is downloaded and geocode is set up
     map_data = load_map_data()
     gc = Geocode()
     gc.load()
 
-    # set up parallel
+    # Set up parallel
     if no_parallel:
         num_cores = 1
     else:
@@ -327,7 +331,7 @@ def run(no_parallel=False, extract_retweets=True, extract_quotes=True, extend=Fa
     logger.info(f'Using {num_cores} CPUs to parse data...')
     parallel = joblib.Parallel(n_jobs=num_cores)
 
-    # run
+    # Run
     logger.info('Extract tweets...')
     extract_tweets_delayed = joblib.delayed(extract_tweets)
     parallel(extract_tweets_delayed(key, f_names, keywords) for key, f_names in tqdm(grouped_f_names.items()))
@@ -335,7 +339,7 @@ def run(no_parallel=False, extract_retweets=True, extract_quotes=True, extend=Fa
     interaction_counts = merge_interaction_counts()
     interaction_counts_fname = dump_interaction_counts(interaction_counts)
 
-    # store counts as shared memory
+    # Store counts as shared memory
     if ray_num_cpus is None and no_parallel:
         ray_num_cpus = 1
     ray.init(num_cpus=ray_num_cpus)
@@ -344,42 +348,47 @@ def run(no_parallel=False, extract_retweets=True, extract_quotes=True, extend=Fa
     if extend:
         existing_parquet_keys = [os.path.basename(f_name).split('.parquet')[0][len('parsed_'):] for f_name in existing_parquet_files]
         f_names_intermediary = glob.glob(os.path.join(output_folder, 'preliminary', '*.jsonl'))
+        # List of existing Parquet files to extend
         f_names_intermediary_existing = []
+        # List of new Parquet files to write
         f_names_intermediary_new = []
+        # Fill the above lists
         for f_name in f_names_intermediary:
             if os.path.basename(f_name).split('.jsonl')[0] in existing_parquet_keys:
                 f_names_intermediary_existing.append(f_name)
             else:
                 f_names_intermediary_new.append(f_name)
+        
+        # Extend existing Parquet files
         if len(f_names_intermediary_existing) == 0:
             logger.info('No files to extend.')
         else:
             logger.info(f'Extending {len(f_names_intermediary_existing):,} existing parquet files...')
-            # extend existing parquet files
             res = ray.get([write_parquet_file.remote(f_name, data_id, extend=True) for f_name in tqdm(f_names_intermediary_existing)])
             num_existing_tweets = sum(res)
             logger.info(f'... updated existing {num_existing_tweets:,} tweets in {len(f_names_intermediary_existing):,} parquet files')
     else:
+        # List of new Parquet files to write
         f_names_intermediary_new = glob.glob(os.path.join(output_folder, 'preliminary', '*.jsonl'))
 
-    # write new parquet files
+    # Write new Parquet files
     if len(f_names_intermediary_new) > 0:
         logger.info(f'Writing {len(f_names_intermediary_new):,} new parquet files...')
         res = ray.get([write_parquet_file.remote(f_name, data_id) for f_name in tqdm(f_names_intermediary_new)])
         num_tweets = sum(res)
         logger.info(f'... collected a total of {num_tweets:,} tweets in {len(f_names_intermediary_new):,} parquet files')
     else:
-        logger.info('No new parquet files to write...')
+        logger.info('No new Parquet files to write...')
         num_tweets = 0
 
-    # write used files
+    # Write used files
     logger.info('Writing used files...')
     if extend:
         # Get full file list
         grouped_f_names = generate_file_list(extend=False, omit_last_day=omit_last_day)
     write_used_files(grouped_f_names)
 
-    # cleanup
+    # Cleanup
     preliminary_folder = os.path.join(output_folder, 'preliminary')
     if os.path.isdir(preliminary_folder):
         logger.info('Cleaning up intermediary files...')
